8fb89d2dd4930bb0d0e9d43aa580c106e273f5da
huydev95 <huydev95@users.noreply.github.com>
2026-09-02 14:44:31 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0xf40592daacb3e5abf358789f5688c0b4f64d7777',
+    name: `FLORK`,
+    symbol: 'FLORK',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0x66661c7229901f568f16bd1551b3ba826f83ce49',
     name: `Teller`,
     symbol: 'DEBIT',
